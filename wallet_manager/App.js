f5882fec5bce36dfe4fc9f87bc50b26e7bfee459
Felipe Cabral <78245415+FelipeCabralz@users.noreply.github.com>
2022-05-25 16:35:09 -0300

    Update App.js

--- wallet_manager/App.js
+++ wallet_manager/App.js
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { AuthProvider } from './contexts/AuthContext';
-import MainNavigator from './routes/MainNavigator';
 import Splash from './screens/Splash';
+import AppNavigator from './app.navigator';
 
 export default function App() {
   const [exibeSplash, setExibeSplash] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => setExibeSplash(false), 3000);
+    setTimeout(() => setExibeSplash(false), 0);
   }, []);
 
   return (
     <AuthProvider>
-      {exibeSplash ? <Splash /> : <MainNavigator />}
+      {exibeSplash ? <Splash /> : <AppNavigator />}
     </AuthProvider>
   );
 }
